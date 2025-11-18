4115d077b8424f68067b05fb7032ef55e8c24707
Aditya Sharma <subhaiyash@gmail.com>
2025-11-18 10:50:48 +0530

    backend setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get( "/api/message", (req,res) => {
+    res.json({messge: "Hello guys"})
+});
+
+const PORT = 4000;
+
+app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
