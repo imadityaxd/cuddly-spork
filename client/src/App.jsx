a27d3f5aea651da2cde819c160518732d1402c2a
Aditya Sharma <subhaiyash@gmail.com>
2025-11-19 15:02:48 +0530

    dockerized

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => {
+        console.error("Error fetching message", err);
+    });
+  }, []);
+  console.log({message});
+
+  return (
+    <>
+    <h1>starting the project</h1>
+    <h2>data: {message}</h2>
+    </>
+    
+  );
+}
+
+export default App
--- server/index.js
+++ server/index.js
@@ -3,15 +3,26 @@ import cors from "cors"
 
 const app = express()
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors(
+    {
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:3000",
+            //add production url
+        ],
+        credentials: true,
+    }),
+);
 
+//API route
 app.get( "/api/message", (req,res) => {
-    res.json({messge: "Hello guys"})
+    res.json({message: "Hello guys"})
 });
 
 const PORT = 4000;
 
-app.listen(PORT, () => 
-    console.log(`Server is running at http://localhost:${PORT}`)
+app.listen(PORT,"0.0.0.0", () => 
+    console.log(`Server is running at http://0.0.0.0:${PORT}`)
 )
